325672fd1f11e727b105df0fad8ba68bd6284ef5
Ecarus1 <solodovnikov.andrey99@mail.ru>
2021-07-19 03:31:39 +0300

    html v0.0.2

--- insure/public/js/clock.js
+++ insure/public/js/clock.js
@@ -1,10 +1,7 @@
-function clock()
-{
-    var date = new Date(),
-            hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
-            minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
-            seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
-    document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
-}
-setInterval(clock, 1000);
-clock();
+window.onload = function() {
+    window.setInterval(function(){
+    var now = new Date();
+    var clock = document.getElementById("clock");
+    clock.innerHTML = now.toLocaleTimeString();
+    },1000);
+};
